97a37cd1b3dc5500265a64a41a02140d7b0b9aec
imapagan <hayappinventor@gmail.com>
2020-12-10 13:58:47 +0300

    Add deploy script and config.

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+# если вы публикуете на пользовательский домен
+# echo 'www.example.com' > CNAME
+
+git init
+git config user.name "imapagan"
+git config user.email "hayappinventor@gmail.com"
+git add -A
+git commit -m 'deploy'
+
+# если вы публикуете по адресу https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:imapagan/appinv_front.git master:gh-pages
+
+cd -
